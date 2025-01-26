97e9044d3043ab5d5acc5acdf3144e066dbf70cd
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-01-26 16:54:43 +0700

    Add POST /file

--- README.md
+++ README.md
@@ -9,3 +9,25 @@
 ```sh
 gvm use go1.23.5
 ```
+
+## go get
+
+```sh
+go get .
+```
+
+## go run
+
+```sh
+go run .
+```
+
+## cURL
+
+- POST /file
+
+```sh
+curl --location 'localhost:8080/file' \
+--form 'file=@"example.png"' \
+--header "Content-Type: multipart/form-data"
+```
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func postFileHandler(context *gin.Context) {
+    file, _ := context.FormFile("file")
+    var dst string
+    context.SaveUploadedFile(file, dst)
+    context.JSON(
+        http.StatusOK,
+        gin.H {
+            "file": file.Filename,
+        },
+    )
+}
+
+func main() {
+    engine := gin.Default()
+    engine.MaxMultipartMemory = 8 << 20
+    engine.POST("/file", postFileHandler)
+    engine.Run()
+}
